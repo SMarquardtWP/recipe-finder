e886512728e46e5b621741e357a2942fd74e024d
Seth Marquardt <marq5299@gmail.com>
2019-02-04 12:37:43 -0500

    minor text updates

--- scripts.js
+++ scripts.js
@@ -2,3 +2,27 @@
 
 // Spoonacular key = 96256b7920msh83769b864ea6c5ep1396eejsn338f4bd8a887
 
+//generates a list of all fetched recipes and displays them for the user
+function displayRecipe(){
+
+}
+
+//accesses API to retrieve a single random recipe for user. Only filters by cuisine
+function randomRecipe(){
+
+}
+
+//accesses API to get information about different recipes based on advanced data received from user
+function advancedRecipes(){
+
+}
+
+//accesses API to get information about different recipes based on basic data received from user
+function basicRecipes(){
+
+}
+
+//watches for form submissions
+function watchForms(){
+
+}
