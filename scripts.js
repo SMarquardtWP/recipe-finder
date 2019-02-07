b28caae25cfbf4bea25ae639275a17bda852f427
SMarquardtWP <45657169+SMarquardtWP@users.noreply.github.com>
2019-02-07 12:45:33 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// Spoonacular key = 96256b7920msh83769b864ea6c5ep1396eejsn338f4bd8a887
+
 
 //generates a list of all fetched recipes and displays them for the user
 function displayRecipe(){
