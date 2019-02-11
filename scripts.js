c8374310acdeb854cb7a9bfbb58b27c826b5b483
Seth Marquardt <marq5299@gmail.com>
2019-02-11 09:04:59 -0500

    basic recipe search and display fully functional

--- scripts.js
+++ scripts.js
@@ -5,70 +5,73 @@ const SETTINGS = {
         'X-RapidAPI-Key': '96256b7920msh83769b864ea6c5ep1396eejsn338f4bd8a887'
     }
 };
-
 const BASE_URL = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes';
-
-function formatQuery(params) {
-
-}
 
 //generates a list of all fetched recipes and displays them for the user
 function displayRecipe(jsonRecipe) {
+    console.log(jsonRecipe);
+    $('.recipeResults').empty();
     $('.recipeResults').append(`
-    <img src='${jsonRecipe.image}' alt='${jsonRecipe.alt}'>
-    <a href='${jsonRecipe.url}'>${jsonRecipe.url}</a><br>`)
+    <h4>${jsonRecipe.title}</h4>
+    <img src='${jsonRecipe.image}'>
+    <a href='${jsonRecipe.sourceUrl}'>${jsonRecipe.sourceUrl}</a><br>`);
 }
 
 //accesses API to retrieve a single random recipe for user. Only filters by cuisine
-function randomRecipe() {
-
+function randomRecipe(url) {
+    fetch(`${url}/random?number=1`)
+    .then(response => response.json())
+    .then(responseJson => findRecipeByID(url,responseJson));
 }
 
 //accesses API to get information about different recipes based on advanced data received from user
 function advancedRecipes(url, cuisine, query, ingredients) {
     fetch(`${url}/search?cuisine=${cuisine}&number=10&offset=0&query=${query}`, SETTINGS)
-    .then(response => response.json())
-    .then(responseJson => callback(responseJson));
+        .then(response => response.json())
+        .then(responseJson => callback(responseJson));
 }
 
 //accesses API to get information about different recipes based on basic data received from user
-function basicRecipes(url, cuisine, query) {
-    fetch(`${url}search?cuisine=${cuisine}&number=10&offset=0&query=${query}`, SETTINGS)
-    .then(response => response.json())
-    .then(responseJson => findRecipeByID(url, responseJson));
-}
-
-function findRecipeByID(url,responseJsonID) {
-    for(let i =0; i<responseJsonID.results.length; i++)
-    fetch(`${url}/${responseJsonID.results[i].id}/information`, SETTINGS)
+function basicRecipes(url, cuisine, query, callback) {
+    fetch(`${url}/search?cuisine=${cuisine}&number=10&offset=0&query=${query}`, SETTINGS)
         .then(response => response.json())
-        .then(responseJson => displayRecipe(responseJson));
+        .then(responseJson => findRecipeByID(url, responseJson)); 
 }
 
-//watches for form submissions
-function watchBasic() {
-    $('basicSearch').submit(event => {
-        let url = BASE_URL;
-
-        let cuisine = $('.cuisineTypes').children("option:selected").val();
-        let query = $('.queries').val();
-
-        findRecipeByID(url,recipeID);
-    });
+//takes recipe ID found by either basicRecipes or advancedRecipes and looks up more detailed information, then calls for it to be displayed
+function findRecipeByID(url, responseJsonID) {
+    for (let i = 0; i < responseJsonID.results.length; i++) {
+        fetch(`${url}/${responseJsonID.results[i].id}/information`, SETTINGS)
+            .then(response => response.json())
+            .then(responseJson => displayRecipe(responseJson));
+    }
 }
 
-function formatIngredients(){
-    let ingredients= '';
-    for (let i=0; i<5; i++){
-        if ($('cleanPantry').find('input').eq(i).val == ''){
+// combines ingredients list from Clean Out the Pantry form into one string to be inserted into url, returns string
+function formatIngredients() {
+    let ingredients = '';
+    for (let i = 0; i < 5; i++) {
+        if ($('cleanPantry').find('input').eq(i).val == '') {
             ingredients = `${ingredients}%2c${'cleanPantry'.find('input').eq(i).val}`
         }
     }
     return ingredients;
 }
 
+//watches for form submissions
+function watchBasic() {
+    $('.search1').click(event => {
+        event.preventDefault();
+        let url = BASE_URL;
+        let cuisine = $('.cuisineTypes').children("option:selected").val();
+        let query = $('.queries').val();
+        basicRecipes(url, cuisine, query);
+    });
+}
+
 function watchIngredients() {
-    $('cleanPantry').submit(event => {
+    $('.search2').click(event => {
+        event.preventDefault();
         let url = BASE_URL + 'search?diet=vegetarian&query=burger&excludeIngredients=coconut&intolerances=egg,gluten&number=10&offset=0';
 
         let cuisine = $('.cuisineTypes').children("option:selected").val();
@@ -80,7 +83,8 @@ function watchIngredients() {
 }
 
 function watchRandom() {
-    $('randomRecipe').submit(event => {
+    $('.search3').click(event => {
+        event.preventDefault();
         let url = BASE_URL + 'additional'
     })
 }
