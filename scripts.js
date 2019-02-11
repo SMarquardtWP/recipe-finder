ba03233974d1df90014567bd9da13e8c7ceae57e
Seth Marquardt <marq5299@gmail.com>
2019-02-11 08:07:43 -0500

    added basic and ingredients retrieval functions, added searchByID function

--- scripts.js
+++ scripts.js
@@ -1,28 +1,22 @@
 'use strict'
 
-const apiKey = '96256b7920msh83769b864ea6c5ep1396eejsn338f4bd8a887';
-
-/*
-const baseURL = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?diet=vegetarian&excludeIngredients=coconut&intolerances=egg%2C+gluten&number=10&offset=0&type=main+course&query=burger'
-
-'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ingredients=apples%2Cflour%2Csugar'
-
-'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=1&tags=vegetarian%2Cdessert'
-*/
-
-const options = {
-    headers: new Headers({ 'X-RapidAPI-Key': apiKey })
+const SETTINGS = {
+    headers: {
+        'X-RapidAPI-Key': '96256b7920msh83769b864ea6c5ep1396eejsn338f4bd8a887'
+    }
 };
 
+const BASE_URL = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes';
+
 function formatQuery(params) {
-    const queryItems = Object.keys(params)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-    return queryItems.join('&');
+
 }
 
 //generates a list of all fetched recipes and displays them for the user
 function displayRecipe(jsonRecipe) {
-
+    $('.recipeResults').append(`
+    <img src='${jsonRecipe.image}' alt='${jsonRecipe.alt}'>
+    <a href='${jsonRecipe.url}'>${jsonRecipe.url}</a><br>`)
 }
 
 //accesses API to retrieve a single random recipe for user. Only filters by cuisine
@@ -31,22 +25,70 @@ function randomRecipe() {
 }
 
 //accesses API to get information about different recipes based on advanced data received from user
-function advancedRecipes() {
-
+function advancedRecipes(url, cuisine, query, ingredients) {
+    fetch(`${url}/search?cuisine=${cuisine}&number=10&offset=0&query=${query}`, SETTINGS)
+    .then(response => response.json())
+    .then(responseJson => callback(responseJson));
 }
 
 //accesses API to get information about different recipes based on basic data received from user
-function basicRecipes() {
-
+function basicRecipes(url, cuisine, query) {
+    fetch(`${url}search?cuisine=${cuisine}&number=10&offset=0&query=${query}`, SETTINGS)
+    .then(response => response.json())
+    .then(responseJson => findRecipeByID(url, responseJson));
 }
 
-//receives input from watchForms, determines what type of search the user wanted to do, then calls appropriate function
-function determineSearchType() {
-
+function findRecipeByID(url,responseJsonID) {
+    for(let i =0; i<responseJsonID.results.length; i++)
+    fetch(`${url}/${responseJsonID.results[i].id}/information`, SETTINGS)
+        .then(response => response.json())
+        .then(responseJson => displayRecipe(responseJson));
 }
 
 //watches for form submissions
-function watchForms() {
-    $('.recipeSearch').on('click', determineSearchType)
+function watchBasic() {
+    $('basicSearch').submit(event => {
+        let url = BASE_URL;
 
+        let cuisine = $('.cuisineTypes').children("option:selected").val();
+        let query = $('.queries').val();
+
+        findRecipeByID(url,recipeID);
+    });
 }
+
+function formatIngredients(){
+    let ingredients= '';
+    for (let i=0; i<5; i++){
+        if ($('cleanPantry').find('input').eq(i).val == ''){
+            ingredients = `${ingredients}%2c${'cleanPantry'.find('input').eq(i).val}`
+        }
+    }
+    return ingredients;
+}
+
+function watchIngredients() {
+    $('cleanPantry').submit(event => {
+        let url = BASE_URL + 'search?diet=vegetarian&query=burger&excludeIngredients=coconut&intolerances=egg,gluten&number=10&offset=0';
+
+        let cuisine = $('.cuisineTypes').children("option:selected").val();
+        let query = $('.queries').val();
+        let ingredients = formatIngredients();
+
+        findRecipeByID(url, getBySearch)
+    })
+}
+
+function watchRandom() {
+    $('randomRecipe').submit(event => {
+        let url = BASE_URL + 'additional'
+    })
+}
+
+function watchAllForms() {
+    watchBasic();
+    watchIngredients();
+    watchRandom();
+}
+
+watchAllForms();
